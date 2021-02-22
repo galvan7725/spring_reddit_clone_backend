5bfe59215f1d3744bf3885eb1969cc3999bbec10
Luis Galvan <galvan7725@gmail.com>
2021-02-22 15:04:28 -0600

    Model

---
+++ src/main/java/com/luisgalvan/springboot/app/model/Comment.java
@@ -0,0 +1,32 @@
+package com.luisgalvan.springboot.app.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
