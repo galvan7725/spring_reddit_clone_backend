7d8bbf6e36b8c1dd5d0e3c96a5aa556d15ee903e
galvan9001 <46407403+galvan9001@users.noreply.github.com>
2021-05-05 18:35:31 -0500

    Security update for jwt

--- src/main/java/com/luisgalvan/springboot/app/config/WebConfig.java
+++ src/main/java/com/luisgalvan/springboot/app/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.luisgalvan.springboot.app.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,6 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600L)
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
-                .allowCredentials(true);
+                .allowCredentials(false);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
